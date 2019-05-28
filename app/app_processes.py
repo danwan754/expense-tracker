94e50ac23a89ada8837070a886d4d05438453766
dwa76 <danwan754@gmail.com>
2019-05-27 18:59:32 -0700

    Add tests for getWeekBudgetRemaining

---
+++ app/app_processes.py
@@ -0,0 +1,150 @@
+from models import Expense, Budget
+from . import db
+
+from datetime import datetime, timedelta
+from calendar import monthrange, isleap
+from sqlalchemy.sql import func
+
+
+
+def getTodayBudgetRemaining(budget, id):
+
+    """
+    Params:
+        budget: A Budget object.
+        id: user ID.
+    Returns the remaining budget for today.
+    """
+
+    todayDate = datetime.now().date()
+
+    todayExpenses = Expense.query.with_entities(func.sum(Expense.cost).label('total')).filter(Expense.user_id==id, Expense.date==todayDate).scalar()
+
+    if not todayExpenses:
+        todayExpenses = 0
+
+    # remaining budget for today
+    todayBudgetRemain = budget.daily - todayExpenses
+
+    return todayBudgetRemain
+
+
+def getWeekBudgetRemaining(budget, id):
+
+    """
+    Param:
+        budget: A Budget object.
+        id: user ID.
+    Returns the remaining budget for the current week.
+    """
+
+    todayDate = datetime.now().date()
+
+    # today's weekday as an integer where Monday = 0, Sunday = 6
+    todayWeekDay = todayDate.weekday()
+
+    # weekday of the starting day of week as the budget creation day
+    startWeekDay = budget.creation_date.weekday()
+
+    # number of days that today is ahead of start day
+    numDays = 0
+
+    if todayWeekDay == startWeekDay:
+        startDate = todayDate
+    elif todayWeekDay > startWeekDay:
+        numDays = todayWeekDay - startWeekDay
+        startDate = datetime.now().date() - timedelta(days=numDays)
+    else:
+        numDays = 7 - (startWeekDay - todayWeekDay)
+        startDate = datetime.now().date() - timedelta(days=numDays)
+
+    # this week's expenses
+    weekExpenses = Expense.query.with_entities(func.sum(Expense.cost).label('total')).filter_by(user_id=id).filter(Expense.date >= startDate, Expense.date <= todayDate).scalar()
+
+    if not weekExpenses:
+        weekExpenses = 0
+
+    # remaining weekly budget
+    weekBudgetRemain = (budget.daily * 7) - weekExpenses
+
+    return weekBudgetRemain
+
+
+def getMonthBudgetRemaining(budget, id):
+
+    """
+    Param:
+        budget: A Budget Object.
+        id: user ID.
+    Returns the remaining budget for the current month.
+    """
+
+    todayDate = datetime.now().date()
+    todayMonth = todayDate.month()
+    todayYear = todayDate.year()
+
+    totalMonthExpenses = Expense.query.with_entities(func.sum(Expense.cost).label('total')).filter_by(user_id=id).filter(Expense.date.month()==todayMonth).scalar()
+
+    # number of days in this month
+    numDays = monthrange(todayYear, todayMonth)
+
+    monthBudget = budget.daily * numDays
+
+    monthBudgetRemain = monthBudget - totalMonthExpenses
+
+    return monthBudgetRemain
+
+
+def getYearBudgetRemaining(budget, id):
+
+    """
+    Param:
+        budget: A Budget object.
+        id: user ID.
+    Returns the remaining budget for the current year.
+    """
+
+    todayDate = datetime.now().date()
+    todayYear = todayDate.year()
+    numDaysInYear = 365
+
+    totalYearExpenses = Expense.query.with_entities(func.sum(Expense.cost).label('total')).filter_by(user_id=id).filter(Expense.date.year()==todayYear).scalar()
+
+    if (isleap(todayYear)):
+        numDaysInYear = 366
+
+    yearBudget = budget.daily * numDaysInYear
+
+    yearBudgetRemain = yearBudget - totalYearExpenses
+
+    return yearBudgetRemain
+
+
+def getAllBudgetsRemaining(budget, id):
+
+    """
+    Param:
+        budget: A Budget object.
+        id: user ID.
+    Returns the remaining budgets in dictionary like: {'today': [value], 'week': [value], 'month': [value], 'year': [value]}
+    """
+
+    # get the remaining budgets
+    todayBudgetRemain = getTodayBudgetRemaining(budget, id)
+    weekBudgetRemain = getWeekBudgetRemaining(budget, id)
+    monthBudgetRemain = getMonthBudgetRemaining(budget, id)
+    yearBudgetRemain = getYearBudgetRemaining(budget, id)
+
+    allBudgetsRemain = {'today': todayBudgetRemain,
+                        'week': weekBudgetRemain,
+                        'month': monthBudgetRemain,
+                        'year': yearBudgetRemain}
+
+    return allBudgetsRemain
+
+
+
+# # sum of expenses for each day
+# dailyExpensesList = Expense.query.with_entities(Expense.date, func.sum(Expense.cost)).group_by(Expense.date).order_by(Expense.date.desc()).all()
+
+# for expenseTuple in dailyExpensesList:
--- test_app_processes.py
+++ test_app_processes.py
@@ -2,7 +2,7 @@
 
 import unittest
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flask_testing import TestCase
 from flask import abort, url_for
@@ -15,6 +15,10 @@ from app import app_processes
 class TestBase(TestCase):
 
     TEST_DB = 'test.db'
+    email = 'test_email@gmail.com'
+    password = 'test_password'
+    user_id = 1
+
 
     def create_app(self):
         config_name = 'testing'
@@ -27,6 +31,9 @@ class TestBase(TestCase):
 
     def setUp(self):
         db.create_all()
+        user = User(email=self.email, password=self.password)
+        db.session.add(user)
+        db.session.commit()
 
 
     def tearDown(self):
@@ -34,32 +41,147 @@ class TestBase(TestCase):
         db.drop_all()
 
 
+
 class TestAppProcesses(TestBase):
 
-    def test_getTodayBudgetRemaining(self):
+    #####
+    # Helper methods
+    #####
+
+    def create_budget(self, user_id, dailyBudget, creationDate):
         """
-        Test that today's remaining budget is correct
+        Returns a budget object
         """
-
-        expected = 50
-
-        test_email = 'test_email@gmail.com'
-        test_password = 'test_password'
-
-        user = User(email=self.test_email, password=self.test_password)
-        db.session.add(user)
-        db.session.commit()
-
-        user_id = User.query.entities_with(User.id).filter_by(email=test_email)
 
         budget = Budget(user_id=user_id,
-                        daily=50,
-                        creation_date=datetime.now().date)
+                        daily=dailyBudget,
+                        creation_date=creationDate)
 
-        todayBudgetRemaining = app_processes.getTodayBudgetRemaining(budget, user_id)
+        return budget
 
+
+    def create_and_save_expense(self, user_id, item, cost, category, date):
+        """
+        Create an expense object and save it to database
+        """
+
+        expense = Expense(user_id=user_id,
+                          item=item,
+                          cost=cost,
+                          category=category,
+                          date=date)
+        db.session.add(expense)
+        db.session.commit()
+
+
+    def get_user_id(self, email):
+        """
+        Return user id
+        """
+
+        return User.query.with_entities(User.id).filter_by(email=email).scalar()
+
+
+
+class TestGetTodayBudgetRemaining(TestAppProcesses):
+
+    dailyBudget = 50
+
+    def test_getTodayBudgetRemaining_with_no_expenses(self):
+        """
+        Test that today's remaining budget is fully available with no expenses
+        today, but an expense for yesterday exists
+        """
+
+        expected = self.dailyBudget
+        cost = 2.50
+        date = datetime.now().date() - timedelta(days=1)
+
+        budget = self.create_budget(self.user_id, self.dailyBudget, datetime.now().date())
+        self.create_and_save_expense(self.user_id, "coffee", cost, "Food", date)
+
+        todayBudgetRemaining = app_processes.getTodayBudgetRemaining(budget, self.user_id)
         self.assertEqual(todayBudgetRemaining, expected)
 
+
+    def test_getTodayBudgetRemaining_with_expenses(self):
+        """
+        Test that today's remaining budget is partially available with expenses
+        today
+        """
+
+        cost1 = 2.50
+        cost2 = 12.25
+        date = datetime.now().date()
+
+        # expected = 50 - 2.50 - 12.25 = 35.25
+        expected = self.dailyBudget - cost1 - cost2
+
+        budget = self.create_budget(self.user_id, self.dailyBudget, datetime.now().date())
+        self.create_and_save_expense(self.user_id, "coffee", cost1, "Food", date)
+        self.create_and_save_expense(self.user_id, "lunch", cost2, "Food", date)
+
+        todayBudgetRemaining = app_processes.getTodayBudgetRemaining(budget, self.user_id)
+        self.assertEqual(todayBudgetRemaining, expected)
+
+
+
+class TestGetWeekRemainingBudget(TestAppProcesses):
+
+    dailyBudget = 50
+    weekBudget = dailyBudget * 7
+    cost1 = 2.50
+    cost2 = 12.25
+
+    def test_getWeekBudgetRemaining_with_today_as_start_of_week(self):
+        """
+        Test that this week's remaining budget is correct with the start of week
+        being today and expenses exists for today and yesterday.
+        Only today's expenses should be used to determine budget remaining.
+        """
+
+        today = datetime.now().date()
+        budgetCreationDate = today
+        expected = self.weekBudget
+
+        budget = self.create_budget(self.user_id, self.dailyBudget, budgetCreationDate)
+        weekBudgetRemaining = app_processes.getWeekBudgetRemaining(budget, self.user_id)
+        self.assertEqual(weekBudgetRemaining, expected)
+
+        date1 = datetime.now().date()
+        date2 = date1 - timedelta(days=1)
+
+        # expected = 350 - 2.50 = 347.50
+        expected = self.weekBudget - self.cost1
+
+        self.create_and_save_expense(self.user_id, "coffee", self.cost1, "Food", date1)
+        self.create_and_save_expense(self.user_id, "lunch", self.cost2, "Food", date2)
+
+        weekBudgetRemaining = app_processes.getWeekBudgetRemaining(budget, self.user_id)
+        self.assertEqual(weekBudgetRemaining, expected)
+
+
+    def test_getWeekBudgetRemaining_with_yesterday_as_start_of_week(self):
+        """
+        Test that this week's remaining budget is correct with the start of week
+        being yesterday and expenses exists for yesterday and today.
+        Expenses for yesterday and today should determine budget remaining.
+        """
+
+        date1 = datetime.now().date()
+        date2 = datetime.now().date() - timedelta(days=1)
+        budgetCreationDate = date2
+
+
+        # expected = 50 - 2.50 - 12.25 = 35.25
+        expected = self.weekBudget - self.cost1 - self.cost2
+
+        budget = self.create_budget(self.user_id, self.dailyBudget, budgetCreationDate)
+        self.create_and_save_expense(self.user_id, "coffee", self.cost1, "Food", date1)
+        self.create_and_save_expense(self.user_id, "lunch", self.cost2, "Food", date2)
+
+        weekBudgetRemaining = app_processes.getWeekBudgetRemaining(budget, self.user_id)
+        self.assertEqual(weekBudgetRemaining, expected)
 
 
 if __name__ == '__main__':
